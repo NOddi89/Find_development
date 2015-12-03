a3a3a084f8330456a74e2c4c04a342727692bc1d
NOddi89 <oooktopus@hotmail.com>
2015-12-03 23:29:11 +0100

    Adding files first time
    Adding all files from local storage

---
+++ Assets/Scripts/AdjacencyList.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjacencyList : CollectionBase 
+{
+	protected internal virtual void Add(EdgeToNeighbor e)
+	{
+		base.InnerList.Add (e);
+	}
+
+	public virtual EdgeToNeighbor this[int index]
+	{
+		get{ return (EdgeToNeighbor) base.InnerList[index]; }
+		set{ base.InnerList[index] = value; }
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour 
+{
+
+	public int m_TileId = -1;
+
+	private Vector3 m_TilePlayerPos;
+	private Collider m_Collider;
+
+
+	private void Awake()
+	{
+		m_Collider = GetComponentInChildren<Collider> ();
+		m_TilePlayerPos = m_Collider.bounds.center + new Vector3(0f, 0.55f, 0f); // Adding some of the players hight to get him above the ground
+	}
+
+	public int TileID
+	{
+		get{ return m_TileId; }
+		set{ m_TileId = value; }
+	}
+
+	public Vector3 GetTilePlayerPos()
+	{
+		return m_TilePlayerPos;
+	}
+
+}
